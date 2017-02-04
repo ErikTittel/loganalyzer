c77d16f1033da997449c6e4d292a2e1bb8bb8d0e
Erik Tittel <et23@live.de>
2017-02-04 17:01:14 +0100

    write results to files

--- src/main/java/loganalyzer/LogAnalyzer.java
+++ src/main/java/loganalyzer/LogAnalyzer.java
@@ -1,9 +1,12 @@
 package loganalyzer;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,63 +15,100 @@ import java.util.stream.Collectors;
 public class LogAnalyzer {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
-        System.out.println("LogAnalyzer analyzes file");
+        String path = "c:/dev/proj/loganalyzer/logs";
 
-        File file = new File(LogAnalyzer.class.getResource("/agentservice-1-ypgly.log").toURI());
+        List<Path> filePaths = Files.find(Paths.get(path), 1, (path_, attr) -> String.valueOf(path_).endsWith(".log")
+        ).filter(Files::isRegularFile).collect(Collectors.toList());
 
-        List<String> lines = Files.readAllLines(file.toPath());
+        StringBuilder summary = new StringBuilder();
 
-        int errorCount = 0;
 
-        Map<String, Error> errors = new HashMap<>();
+        for (Path filePath : filePaths) {
 
-        int currentLine = 0;
-        while (currentLine < lines.size()) {
-            if (lines.get(currentLine).contains("ERROR")) {
-                errorCount++;
+            String filename = filePath.toString();
+            String headline = "LogAnalyzer analyzes file '" + filename + "'";
+            System.out.println(headline);
+            summary.append("===========================================\n");
+            summary.append(filename.substring(filename.lastIndexOf('\\') + 1)).append("\n");
+            summary.append("===========================================\n");
 
-                currentLine++;
-                if (currentLine >= lines.size()) {
-                    break;
-                }
-                if (!lines.get(currentLine).equals("")) {
-                    Error noExceptionError = errors.get("no Exception");
-                    if (noExceptionError == null) {
-                        errors.put("no Exception", new Error("[no exception]", "[no stack trace]", currentLine - 1));
+            File file = new File("/" + filename);
+
+            List<String> lines = Files.readAllLines(file.toPath());
+
+            int errorCount = 0;
+
+            Map<String, Error> errors = new HashMap<>();
+
+            int currentLine = 0;
+            while (currentLine < lines.size()) {
+                if (lines.get(currentLine).contains("ERROR")) {
+                    errorCount++;
+
+                    currentLine++;
+                    if (currentLine >= lines.size()) {
+                        break;
+                    }
+                    if (!lines.get(currentLine).equals("")) {
+                        Error noExceptionError = errors.get("no Exception");
+                        if (noExceptionError == null) {
+                            errors.put("no Exception", new Error("[no exception]", "[no stack trace]", currentLine -
+                                    1));
+
+                        } else {
+                            noExceptionError.addLineNumber(currentLine - 1);
+                        }
+                        continue;
+                    }
+
+                    currentLine++;
+                    String exception = lines.get(currentLine);
+                    Error error = errors.get(exception);
+                    if (error == null) {
+                        int exceptionLine = currentLine;
+                        StringBuilder stacktrace = new StringBuilder();
+                        String line = lines.get(currentLine);
+                        while (!"".equals(line) && currentLine + 1 < lines.size()) {
+                            stacktrace.append(line).append("\n");
+                            currentLine++;
+                            line = lines.get(currentLine);
+                        }
+
+                        errors.put(exception, new Error(exception, stacktrace.toString(), exceptionLine + 1));
                     } else {
-                        noExceptionError.addLineNumber(currentLine - 1);
+                        error.addLineNumber(currentLine + 1);
                     }
-                    continue;
                 }
-
-                currentLine ++;
-                String exception = lines.get(currentLine);
-                Error error = errors.get(exception);
-                if (error == null) {
-                    int exceptionLine = currentLine;
-                    StringBuilder stacktrace = new StringBuilder();
-                    String line = lines.get(currentLine);
-                    while (!"".equals(line) && currentLine < exceptionLine + 150 && currentLine + 1 < lines.size()) {
-                        stacktrace.append(line).append("\n");
-                        currentLine++;
-                        line = lines.get(currentLine);
-                    }
-
-                    errors.put(exception, new Error(exception, stacktrace.toString(), exceptionLine + 1));
-                } else {
-                    error.addLineNumber(currentLine + 1);
-                }
+                currentLine++;
             }
-            currentLine++;
+
+            try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(filename + "-summary.txt"))) {
+                writer.write("Log analysis from " + filename + "\n\n");
+                writer.write("Errors: " + errorCount + "\n");
+                errors.forEach((k, v) -> {
+                    try {
+                        writer.write(v.lineNumbers.size() + ": " + k + "\n");
+                        writer.write("Occurrences: " + v.lineNumbers.stream().map(Object::toString).collect
+                                (Collectors.joining(", ")) + "\n");
+                        writer.write(v.stacktrace + "\n\n");
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+
+            summary.append("Errors: ").append(errorCount).append("\n");
+            errors.forEach((k, v) -> {
+                summary.append(v.lineNumbers.size()).append(": ").append(k).append("\n");
+            });
+            summary.append("\n");
         }
 
-        System.out.println("Errors: " + errorCount);
-        errors.forEach((k, v) -> {
-            System.out.println(v.lineNumbers.size() + ": " + k);
-            System.out.println("Occurrences: " + v.lineNumbers.stream().map(Object::toString).collect(Collectors.joining
-                    (", ")));
-            System.out.println(v.stacktrace);
-            System.out.println();
-        });
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(path + "/Log-analysis-summary.txt"))) {
+            writer.append(summary);
+        }
+
+        System.out.println(summary.toString());
+
     }
 }
